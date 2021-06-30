9a7ffbc388e585de7e656930e6b1868829e52ac4
Forklein <giuseppepisani2@gmail.com>
2021-06-30 14:03:57 +0200

    Set comments in js file and add color in style

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-// Test
+// Test JS
 console.log('Ciao Peppe')
 
 //Exercise
@@ -10,10 +10,11 @@ poi chiedi il suo colore preferito
 Infine scrivi sulla pagina nomecognomecolorepreferito21
 */
 
-var user = prompt('Inserisci il tuo nome:');
+//!Variable
+var user = prompt('Inserisci il tuo Nome:');
 var surname = prompt('Inserisci il tuo Cognome:');
-var color = prompt('Inserisci il tuo colore:');
-
+var color = prompt('Inserisci il tuo Colore:');
+//!Function
 document.getElementById('user').innerHTML = user;
 var password = document.getElementById('password');
 password.innerHTML = user + surname + color + '21';

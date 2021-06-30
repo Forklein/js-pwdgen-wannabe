f2be59ba8cc65187fef4d9e360f72ddedfb926b5
Forklein <giuseppepisani2@gmail.com>
2021-06-30 13:59:42 +0200

    Set html, css with container and js function

--- script/script.js
+++ script/script.js
@@ -1,1 +1,19 @@
+// Test
 console.log('Ciao Peppe')
+
+//Exercise
+
+/*
+Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito21
+*/
+
+var user = prompt('Inserisci il tuo nome:');
+var surname = prompt('Inserisci il tuo Cognome:');
+var color = prompt('Inserisci il tuo colore:');
+
+document.getElementById('user').innerHTML = user;
+var password = document.getElementById('password');
+password.innerHTML = user + surname + color + '21';
